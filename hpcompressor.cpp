c75c7b5f076f4319a34b68bb9e137d9d681de310
Van Tuan Le <van-tuan.le@grenoble-inp.org>
2018-01-06 00:24:29 +0100

    [Parallel] Main Program

--- hpcompressor.cpp
+++ hpcompressor.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
+#include "compressor.hpp"
 
-#include "tree.hpp"
+int main(int argc, char* argv[]){
+    
+    if(argc < 4)
+        cout << "Usage: \n \t hpcompressor -c compressed_file_name decompressed_file_name \n \t hpcompressor -d decompressed_file_name compressed_file"<< endl;
 
-using namespace std;
+    if(argv[1][0] == '-' && argv[1][1]== 'c') {
+        Compressor comp = Compressor(argv[2]);
+        comp.generate(argv[3]);
+    }
 
-int main(){
-    cout << "Not implemented ..." << endl;
+    //Compressor comp = Compressor("mini.txt");
+    //Compressor comp = Compressor("shakespeare.txt");
+    //comp.display();
+    else if(argv[1][0] == '-' && argv[1][1]== 'd') {
+        Decompressor decomp = Decompressor(argv[2]);
+        decomp.generate(argv[3]);
+    }
+    else {
+        cout << "Usage: \n \t hpcompressor -c compressed_file_name decompressed_file_name \n \t hpcompressor -d decompressed_file_name compressed_file"<< endl;
+    }
+    //comp = Compressor("sample.txt");
+    //comp.generate("result_comp");
+    //comp.display();
+    //decomp = Decompressor("result_comp.comp");
+    //decomp.generate("result_comp");
 }
